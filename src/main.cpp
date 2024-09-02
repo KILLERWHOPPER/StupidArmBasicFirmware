3403d948a1b7b1d84e9dd3e1667f3805cc4033f9
KILLERWHOPPER <zhang1029946339@gmail.com>
2024-09-02 00:57:42 -0400

    format

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,10 @@
 
 #define USE_MULTCORE 1
 void setup() {
-    Serial.begin(9600);
-init_steppers();
-connect_wifi("KILLERWHOPPER_2.4", "84639188");
-start_server();
+  Serial.begin(9600);
+  init_steppers();
+  connect_wifi("KILLERWHOPPER_2.4", "84639188");
+  start_server();
 }
 
-void loop() {
-    delay(1000);
-
-}
+void loop() { delay(1000); }
